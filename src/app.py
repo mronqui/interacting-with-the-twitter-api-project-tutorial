f0e3d902c8cd968330274e4c4402768e8a21fc1e
Mauricio Ronqui <mronqui@gmail.com>
2022-06-18 00:15:42 +0000

    ejercicio parte 1

--- src/app.py
+++ src/app.py
@@ -1,8 +1,78 @@
+import requests
+import tweepy
 import os
-from dotenv import load_dotenv
+import re
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from dotenv import load_dotenv   #for python-dotenv method
 
 # load the .env file variables
-load_dotenv()
+load_dotenv()                    
+
+consumer_key = os.environ.get('CONSUMER_KEY')
+consumer_secret = os.environ.get('CONSUMER_SECRET')
+access_token = os.environ.get('ACCESS_TOKEN')
+access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+bearer_token = os.environ.get('BEARER_TOKEN')
+
+# Use tweepy.Client()
+client = tweepy.Client( bearer_token=bearer_token, 
+                        consumer_key=consumer_key, 
+                        consumer_secret=consumer_secret, 
+                        access_token=access_token, 
+                        access_token_secret=access_token_secret, 
+                        return_type = requests.Response,
+                        wait_on_rate_limit=True)
+
+#https://api.twitter.com/1.1/direct_messages/events/show.json
+
+# Define query
+query = '#100daysofcode (pandas OR python) -is:retweet'
+
+# get max. 100 tweets
+tweets = client.search_recent_tweets(query=query, 
+                                    tweet_fields=['author_id','created_at','lang'],
+                                     max_results=100)
+
+# Save data as dictionary
+tweets_dict = tweets.json() 
+
+# Extract "data" value from dictionary
+tweets_data = tweets_dict['data'] 
+
+# Transform to pandas Dataframe
+df = pd.json_normalize(tweets_data) 
+
+df.to_csv("coding-tweets.csv")
+
+#define your function here
+def word_in_text(word, text):
+    word = word.lower()
+    text = text.lower()
+    match = re.search(word, text)
+
+    if match:
+        return True
+    return False
 
 
-# your app code here
+# Initialize list to store tweet counts
+[pandas, python] = [0, 0]
+
+# Iterate through df, counting the number of tweets in which each(pandas and python) is mentioned.
+for index, row in df.iterrows():
+    pandas += word_in_text('pandas', row['text'])
+    python += word_in_text('python', row['text'])
+
+# Set seaborn style
+sns.set(color_codes=True)
+
+# Create a list of labels:cd
+cd = ['pandas', 'python']
+
+# Plot the bar chart
+ax = sns.barplot(cd, [pandas, python])
+ax.set(ylabel="count")
+plt.show()
